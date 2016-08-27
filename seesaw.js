abed0cdbbe7fa647b9e3e571058e97a90110208b
Jim MacArthur <jim+git@mode7.co.uk>
2016-08-27 13:04:45 +0100

    Remove some unnecessary demo code from seesaw.js and convert to unix line endings

--- seesaw.js
+++ seesaw.js
@@ -1,5 +1,4 @@
-demos.top = {};
-demos.top.createBall = function(world, x, y, rad, fixed) {
+function createBall(world, x, y, rad, fixed) {
 	var ballSd = new b2CircleDef();
 	if (!fixed) ballSd.density = 1.0;
 	ballSd.radius = rad || 10;
@@ -9,7 +8,8 @@ demos.top.createBall = function(world, x, y, rad, fixed) {
 	ballBd.position.Set(x,y);
 	return world.CreateBody(ballBd);
 };
-demos.top.createPoly = function(world, x, y, points, fixed) {
+
+function createPoly(world, x, y, points, fixed) {
 	var polySd = new b2PolyDef();
 	if (!fixed) polySd.density = 1.0;
 	polySd.vertexCount = points.length;
@@ -21,10 +21,11 @@ demos.top.createPoly = function(world, x, y, points, fixed) {
 	polyBd.position.Set(x,y);
 	return world.CreateBody(polyBd)
 };
-demos.top.initWorld = function(world) {
-	demos.top.createBall(world, 350, 100, 50, true);
-	demos.top.createPoly(world, 100, 100, [[0, 0], [10, 30], [-10, 30]], true);
-	demos.top.createPoly(world, 150, 150, [[0, 0], [10, 30], [-10, 30]], true);
+
+function initWorld(world) {
+	createBall(world, 350, 100, 50, true);
+	createPoly(world, 100, 100, [[0, 0], [10, 30], [-10, 30]], true);
+	createPoly(world, 150, 150, [[0, 0], [10, 30], [-10, 30]], true);
 	var pendulum = createBox(world, 150, 100, 20, 20, false);
 	var jointDef = new b2RevoluteJointDef();
 	jointDef.body1 = pendulum;
@@ -32,13 +33,11 @@ demos.top.initWorld = function(world) {
 	jointDef.anchorPoint = pendulum.GetCenterPosition();
 	world.CreateJoint(jointDef);
 
-	var seesaw = demos.top.createPoly(world, 300, 200, [[0, 0], [100, 30], [-100, 30]]);
+	var seesaw = createPoly(world, 300, 200, [[0, 0], [100, 30], [-100, 30]]);
 	jointDef.body1 = seesaw;
 	jointDef.anchorPoint = seesaw.GetCenterPosition();
 	world.CreateJoint(jointDef);
 };
-demos.InitWorlds.push(demos.top.initWorld);
-
 
 var initId = 0;
 var world = createWorld();
@@ -48,16 +47,6 @@ var canvasHeight;
 var canvasTop;
 var canvasLeft;
 
-function setupWorld(did) {
-	if (!did) did = 0;
-	world = createWorld();
-	initId += did;
-	initId %= demos.InitWorlds.length;
-	if (initId < 0) initId = demos.InitWorlds.length + initId;
-	demos.InitWorlds[initId](world);
-}
-function setupNextWorld() { setupWorld(1); }
-function setupPrevWorld() { setupWorld(-1); }
 function step(cnt) {
 	var stepping = false;
 	var timeStep = 1.0/60;
@@ -68,24 +57,24 @@ function step(cnt) {
 	setTimeout('step(' + (cnt || 0) + ')', 10);
 }
 Event.observe(window, 'load', function() {
-	setupWorld();
-	ctx = $('canvas').getContext('2d');
-	var canvasElm = $('canvas');
+	world = createWorld();
+    initWorld(world);
+    ctx = $('canvas').getContext('2d');
+    var canvasElm = $('canvas');
+    
 	canvasWidth = parseInt(canvasElm.width);
 	canvasHeight = parseInt(canvasElm.height);
 	canvasTop = parseInt(canvasElm.style.top);
 	canvasLeft = parseInt(canvasElm.style.left);
 	Event.observe('canvas', 'click', function(e) {
-		//setupNextWorld();
 		if (Math.random() < 0.5) 
-			demos.top.createBall(world, Event.pointerX(e) - canvasLeft, Event.pointerY(e) - canvasTop);
+			createBall(world, Event.pointerX(e) - canvasLeft, Event.pointerY(e) - canvasTop);
 		else 
 			createBox(world, Event.pointerX(e) - canvasLeft, Event.pointerY(e) - canvasTop, 10, 10, false);
 	});
 	Event.observe('canvas', 'contextmenu', function(e) {
-		if (e.preventDefault) e.preventDefault();
-		setupPrevWorld();
-		return false;
+	    /* Right click - does nothing. */
+	    return false;
 	});
 	step();
 });
