8958a902836ac469ae17eaa3204d8da835c3efc3
Jim MacArthur <jim+git@mode7.co.uk>
2016-08-28 10:18:30 +0100

    Draw coins instead of circles

--- draw_world.ts
+++ draw_world.ts
@@ -2,6 +2,9 @@ var b2Joint;
 var b2Shape;
 var b2Math;
 
+var coin1Image = new Image();
+coin1Image.src = 'graphics/coin64.png';
+
 function drawWorld(world, context) {
 	for (var j = world.m_jointList; j; j = j.m_next) {
 		drawJoint(j, context);
@@ -57,27 +60,16 @@ function drawShape(shape, context) {
 	switch (shape.m_type) {
 	case b2Shape.e_circleShape:
 		{
-			var circle = shape;
-			var pos = circle.m_position;
-			var r = circle.m_radius;
-			var segments = 16.0;
-			var theta = 0.0;
-			var dtheta = 2.0 * Math.PI / segments;
-			// draw circle
-			context.moveTo(pos.x + r, pos.y);
-			for (var i = 0; i < segments; i++) {
-				var d = new b2Vec2(r * Math.cos(theta), r * Math.sin(theta));
-				var v = b2Math.AddVV(pos, d);
-				context.lineTo(v.x, v.y);
-				theta += dtheta;
-			}
-			context.lineTo(pos.x + r, pos.y);
-	
-			// draw radius
-			context.moveTo(pos.x, pos.y);
-			var ax = circle.m_R.col1;
-			var pos2 = new b2Vec2(pos.x + r * ax.x, pos.y + r * ax.y);
-			context.lineTo(pos2.x, pos2.y);
+		    var circle = shape;
+		    var pos = circle.m_position;
+		    var r = circle.m_radius;
+		    var ax = circle.m_R.col1;
+		    context.save();
+		    var rot = Math.atan2(ax.y,ax.x);
+		    context.translate(pos.x,pos.y);
+		    context.rotate(rot);
+		    context.drawImage(coin1Image, -r, -r, r*2, r*2);
+		    context.restore();
 		}
 		break;
 	case b2Shape.e_polyShape:
